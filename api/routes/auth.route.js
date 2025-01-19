da713c3d4168f74cb8b6beeae5055d71780a3313
DeepBikramRana <jin.mori693@gmail.com>
2025-01-19 20:15:06 +0545

    I’ve created a sign-in API that validates user credentials, compares passwords using bcryptjs, and generates a JWT token for secure sessions.

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router(); // Use express.Router() here
 
 // Define your route
 router.post('/signup', signup);
+// Define your route
+router.post('/signin', signin);
 
 export default router; // Export the router as the default export
