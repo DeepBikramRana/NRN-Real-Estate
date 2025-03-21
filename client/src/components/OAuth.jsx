191dbed3bbfa1cc8522f40825cf2cac26082414d
DeepBikramRana <jin.mori693@gmail.com>
2025-03-21 22:43:25 +0545

    Daily Update

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -4,12 +4,11 @@ import { app } from '../firebase';
 export default function OAuth() {
     const handleGoogleClick = async () => {
         try {
-            
             const provider = new GoogleAuthProvider()
             const auth = getAuth(app)
 
-            const result = await signInWithPopup (auth, provider)
-
+            const result = await signInWithPopup(auth, provider);
+            
             console.log(result);
         } catch (error) {
             console.log('Could not sign in with Google', error);
